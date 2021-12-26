02a164a57b2a429291b4117dc4b9fdafbc20108e
Malte Schink <13069402+m4schini@users.noreply.github.com>
2021-12-26 18:20:58 +0100

    ccpm 1

--- ccpm.lua
+++ ccpm.lua
@@ -1,1 +1,15 @@
+ local args = {...}
  
+ local packages = {
+    trc="",
+    trc_bg="",
+    pocket=""
+ }
+
+
+local instr = args[1]
+if instr == "list" then
+    for k, v in pairs(packages) do
+        print(k)
+    end
+end
